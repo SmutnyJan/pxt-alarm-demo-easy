70a65cc0b633a7c15ef69489eeea1d288589b90a
SmutnyJan <57211415+SmutnyJan@users.noreply.github.com>
2022-04-16 13:43:43 +0200

    Aktualizovat pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,52 @@
-
+alarm.onAlarm(function () {
+    music.playTone(262, music.beat(BeatFraction.Whole))
+})
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        . # # # .
+        # # # # #
+        . . # . .
+        `)
+    alarm.turnOnAlarmAndBroadcast()
+})
+radio.onReceivedString(function (receivedString) {
+    vysledek = alarm.receiveBroadcast(receivedString)
+    if (vysledek == true) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            . # # # .
+            # # # # #
+            . . # . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            . # # # .
+            # # # # #
+            . . . . .
+            `)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        . # # # .
+        # # # # #
+        . . . . .
+        `)
+    alarm.turnOffAlarmAndBroadcast()
+})
+let vysledek = false
+basic.showLeds(`
+    . . # . .
+    . # # # .
+    . # # # .
+    # # # # #
+    . . . . .
+    `)
+radio.setGroup(1)
